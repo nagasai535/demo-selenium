7ca5dfdf524eb51d8387bac6e350cd5beb8fb41b
satish.wingherry <satish.wingherry@gmail.com>
2021-08-29 15:14:47 +0530

    sai changes

---
+++ src/demos/seleniumjava.java
@@ -0,0 +1,20 @@
+package demos;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class seleniumjava {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\tools\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		System.out.println("Welcome  to Ebay...");
+		driver.get("https://www.ebay.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(4000);
+		driver.close();
+
+	}
+
+}
