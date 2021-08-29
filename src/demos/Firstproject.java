b2c846e6aa376def7ed9ea98a1829007d61ccc23
satish.wingherry <satish.wingherry@gmail.com>
2021-08-29 15:04:46 +0530

    sai changes

---
+++ src/demos/Firstproject.java
@@ -0,0 +1,16 @@
+package demos;
+
+import org.testng.annotations.Test;
+
+public class Firstproject {
+
+	@Test
+	public void test1() {
+		System.out.println("Test1");
+	}
+	@Test
+	public void test2() {
+		System.out.println("Test2..");
+	}
+
+}
